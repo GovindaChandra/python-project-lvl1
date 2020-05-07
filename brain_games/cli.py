e1cc0e015b75341ef36aa1c82d46aa9a18d5aa65
GovindaChandra <doyalnitay@gmail.com>
2020-05-07 23:51:43 +0500

    Refactoring

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,9 +4,9 @@ import prompt
 
 
 def welcome_user():
-    print('Welcome to the Brain Games!')
+    print('\nWelcome to the Brain Games!\n')
     name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
+    print('Hello, {0}!\n'.format(name))
 
 
 if __name__ == '__main__':
