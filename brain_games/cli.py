d03a2bfd221fd6793dd355dc5e4741f420bedb6c
GovindaChandra <doyalnitay@gmail.com>
2020-05-15 21:30:51 +0500

    Update in accordance with the added game "Is even?"

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,5 +6,9 @@ import prompt
 def welcome_user():
     """Welcome user function."""
     print('\nWelcome to the Brain Games!\n')
+
+def get_name():
+    """Request user name."""
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!\n'.format(name))
+    return name
