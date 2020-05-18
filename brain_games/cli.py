8e51b669656516a270ef325bbc3c9402a9bf99d0
GovindaChandra <doyalnitay@gmail.com>
2020-05-18 23:15:56 +0500

    Cast in accordance with linter requirements

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,8 +7,13 @@ def welcome_user():
     """Welcome user function."""
     print('\nWelcome to the Brain Games!')
 
+
 def get_name():
-    """Request user name."""
+    """Request user name and return him.
+
+    Returns:
+        return user name.
+    """
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!\n'.format(name))
     return name
