6c0507c770376001ca52b89e4265681f4493d2b0
GovindaChandra <doyalnitay@gmail.com>
2020-05-27 23:36:29 +0500

    Add brain_calc game logic file and script

---
+++ brain_games/game_expression_calculation.py
@@ -0,0 +1,11 @@
+"""Expression calculation game logic."""
+
+import random
+
+import prompt
+
+from brain_games.cli import username_request, welcome_user
+
+
+def expression_calc():
+    pass
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Expression calculation."""
+
+from brain_games.game_expression_calculation import expression_calc
+
+
+def main():
+    """Run Expression calculation brain game."""
+    expression_calc()
+
+
+if __name__ == '__main__':
+    main()
