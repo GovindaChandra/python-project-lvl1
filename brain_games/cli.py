983c1654385ef841dcad5c963bc09c9634f73801
Sergey T <doyalnitay@gmail.com>
2022-04-09 17:52:04 +0500

    remove username_request(), update welcome()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,16 +4,12 @@ import prompt
 
 
 def welcome_user():
-    """Welcome to Brain Games function."""
-    print('\nWelcome to the Brain Games!')
-
-
-def username_request():
-    """Request user name, print a greeting and return him.
+    """Welcome, request user name, print a greeting and return him.
 
     Returns:
         User name.
     """
+    print('\nWelcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!\n'.format(name))
     return name
