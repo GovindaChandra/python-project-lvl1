bd922e6392799f95af1aca90a95e8e61ab4940cf
Sergey T <doyalnitay@gmail.com>
2022-04-24 18:13:08 +0500

    update doctrings

--- brain_games/games/game_iseven.py
+++ brain_games/games/game_iseven.py
@@ -7,11 +7,11 @@ from brain_games.games.game_settings import game_engine
 
 def _brain_even_game_function():
     number = random.randint(1, 100)  # noqa: S311
-    answer = 'yes' if number % 2 == 0 else 'no'
-    return str(number), answer
+    correct_answer = 'yes' if number % 2 == 0 else 'no'
+    return str(number), correct_answer
 
 
 def brain_even():
-    """Start the game."""
+    """Start the brain-even game."""
     game_rule = 'Answer "yes" if number is even, otherwise answer "no".\n'
     game_engine(game_rule, _brain_even_game_function)
