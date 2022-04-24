db9f3b618b40b0526d23cc62c2dfc41149a20277
Sergey T <doyalnitay@gmail.com>
2022-04-24 17:14:51 +0500

    add game_engine function

--- brain_games/games/game_settings.py
+++ brain_games/games/game_settings.py
@@ -1,5 +1,25 @@
 """Global settings for games."""
 
+import prompt
+
+from brain_games.cli import welcome
+
 NUMBER_OF_QUESTIONS = 3
 GAME_OVER = "'{0}' is wrong answer ;(. Correct answer was '{1}'."
 GAME_WIN = 'Congratulations, {0}!\n'
+
+
+def game_engine(game_rule, _game_function):
+    name = welcome(game_rule)
+    for step in range(NUMBER_OF_QUESTIONS):
+        expression, correct_answer = _game_function()
+        print('Question:', expression)
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print('Correct!')
+            if step == NUMBER_OF_QUESTIONS - 1:
+                print(GAME_WIN.format(name))
+        else:
+            print(GAME_OVER.format(answer, correct_answer))
+            print("Let's try again, {0}!\n".format(name))
+            break
