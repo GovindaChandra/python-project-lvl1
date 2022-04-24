be1740a101a214df838c5c62364a0e88316add74
Sergey T <doyalnitay@gmail.com>
2022-04-24 17:22:39 +0500

    taking out repeating function to game_settings.py

--- brain_games/games/game_iseven.py
+++ brain_games/games/game_iseven.py
@@ -2,37 +2,16 @@
 
 import random
 
-import prompt
-
-from brain_games.cli import welcome
-from brain_games.games.game_settings import (
-    GAME_OVER,
-    GAME_WIN,
-    NUMBER_OF_QUESTIONS,
-)
+from brain_games.games.game_settings import game_engine
 
 
-def _is_even(number):
-    return 'yes' if number % 2 == 0 else 'no'
-
-
-def _number_gen():
-    return random.randint(1, 100)  # noqa: S311
+def _brain_even_game_function():
+    number = random.randint(1, 100)  # noqa: S311
+    answer = 'yes' if number % 2 == 0 else 'no'
+    return str(number), answer
 
 
 def brain_even():
-    """Brain even game logic."""
-    name = welcome('Answer "yes" if number is even, otherwise answer "no".\n')
-
-    for step in range(NUMBER_OF_QUESTIONS):
-        number = _number_gen()
-        print('Question:', number)
-        answer = prompt.string('Your answer: ')
-        if answer == _is_even(number):
-            print('Correct!')
-            if step == NUMBER_OF_QUESTIONS - 1:
-                print(GAME_WIN.format(name))
-        else:
-            print(GAME_OVER.format(answer, _is_even(number)))
-            print("Let's try again, {0}!\n".format(name))
-            break
+    """Start the game."""
+    game_rule = 'Answer "yes" if number is even, otherwise answer "no".\n'
+    game_engine(game_rule, _brain_even_game_function)
