414ead618a244acf8b4c2a9bd204685946de75f3
Sergey T <doyalnitay@gmail.com>
2022-04-24 17:16:36 +0500

    parameter rename, and correcting docstrings

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,11 +3,11 @@
 import prompt
 
 
-def welcome(game_message):
+def welcome(game_rule):
     """Welcome, request user name, print a greeting and return him.
 
     Parameters:
-        game_message: Describe game rules
+        game_rule: Describe game rules
 
     Returns:
         name: User name
@@ -15,5 +15,5 @@ def welcome(game_message):
     print('\nWelcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!\n'.format(name))
-    print(game_message)
+    print(game_rule)
     return name
