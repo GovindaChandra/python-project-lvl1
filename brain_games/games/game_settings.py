dc61c86c13f44240b3e62cc23d245374f03a99f4
Sergey T <doyalnitay@gmail.com>
2022-04-24 18:13:40 +0500

    add docstrings to game_engine function

--- brain_games/games/game_settings.py
+++ brain_games/games/game_settings.py
@@ -9,10 +9,16 @@ GAME_OVER = "'{0}' is wrong answer ;(. Correct answer was '{1}'."
 GAME_WIN = 'Congratulations, {0}!\n'
 
 
-def game_engine(game_rule, _game_function):
+def game_engine(game_rule, game_function):
+    """Call greeting, lunch game function.
+
+    Parameters:
+        game_rule: Describe game rules
+        game_function: game logic function
+    """
     name = welcome(game_rule)
     for step in range(NUMBER_OF_QUESTIONS):
-        expression, correct_answer = _game_function()
+        expression, correct_answer = game_function()
         print('Question:', expression)
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
