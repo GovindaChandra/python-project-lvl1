186fbb1bb53e3329f88bf23f532c68ae0f2b708d
Sergey T <doyalnitay@gmail.com>
2022-04-24 19:37:12 +0500

    move logic game to game_gcd file

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,20 +1,14 @@
-"""Greatest common divisor game logic."""
+#!/usr/bin/env python
 
-import math
-import random
+"""Game the correct answer is an even or odd number."""
 
-from brain_games.games.game_settings import game_engine
-
-
-def _brain_gcd_game_function():
-    first_op = random.randint(1, 100)  # noqa: S311
-    second_op = random.randint(1, 100)  # noqa: S311
-    correct_answer = math.gcd(first_op, second_op)
-    expression = '{0} {1}'.format(first_op, second_op)
-    return expression, str(correct_answer)
+from brain_games.games.game_gcd import brain_gcd
 
 
-def brain_gcd():
-    """Start the brain-gcd game."""
-    game_rule = 'Find the greatest common divisor of given numbers.\n'
-    game_engine(game_rule, _brain_gcd_game_function)
+def main():
+    """Run greatest common divisior brain game."""
+    brain_gcd()
+
+
+if __name__ == '__main__':
+    main()
