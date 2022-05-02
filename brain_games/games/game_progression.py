621d96b655b0475c79ab4966aa52332aa1e2a1f4
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-03 00:12:09 +0500

    updating the generated value 'diff'

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -8,7 +8,7 @@ from brain_games.games.game_settings import game_engine
 def _brain_progression_game_function():
     count_nums = random.randint(5, 10)  # noqa: S311
     first_num = random.randint(1, 100)  # noqa: S311
-    diff = random.randint(-count_nums, count_nums)  # noqa: S311
+    diff = random.randint(1, count_nums) * random.choice([-1, 1])  # noqa: S311
     progression = list(range(first_num, first_num + count_nums * diff, diff))
     correct_answer = progression[abs(diff) - 1]
     progression[abs(diff) - 1] = '..'
