176b12aae150dfb7dba931ce2b828697398df379
Sergey T <doyalnitay@gmail.com>
2022-05-02 23:26:26 +0500

    Create 2 files for progression game

---
+++ brain_games/games/game_progression.py
@@ -0,0 +1,21 @@
+"""Arithmetic progression game logic."""
+
+import random
+
+from brain_games.games.game_settings import game_engine
+
+
+def _brain_progression_game_function():
+    count_nums = random.randint(5, 10)  # noqa: S311
+    first_num = random.randint(1, 100)  # noqa: S311
+    diff = random.randint(-count_nums, count_nums)  # noqa: S311
+    progression = list(range(first_num, first_num + count_nums * diff, diff))
+    correct_answer = progression[abs(diff) - 1]
+    progression[abs(diff) - 1] = '..'
+    return ' '.join(map(str, progression)), str(correct_answer)
+
+
+def brain_progression():
+    """Start the brain-gcd game."""
+    game_rule = 'What number is missing in the progression?\n'
+    game_engine(game_rule, _brain_progression_game_function)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Game the correct answer is an even or odd number."""
+
+from brain_games.games.game_progression import brain_progression
+
+
+def main():
+    """Run greatest common divisior brain game."""
+    brain_progression()
+
+
+if __name__ == '__main__':
+    main()
