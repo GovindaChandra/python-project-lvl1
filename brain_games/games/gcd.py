60313436260fc735dfe3c9aaaa6437f79284d21c
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-18 22:17:15 +0500

    replesement random module to secrets, rename function, add string constant for game rule

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,20 +1,22 @@
 """Greatest common divisor game logic."""
 
 import math
-import random
+import secrets
 
 from brain_games.game_engine.game_engine import start_game
 
+GAME_RULE = 'Find the greatest common divisor of given numbers.\n'
 
-def _brain_gcd_game_function():
-    first_op = random.randint(1, 100)  # noqa: S311
-    second_op = random.randint(1, 100)  # noqa: S311
-    correct_answer = math.gcd(first_op, second_op)
-    expression = '{0} {1}'.format(first_op, second_op)
+
+def _game_data_generation():
+    secrets_generator = secrets.SystemRandom()
+    num1 = secrets_generator.randint(1, 100)
+    num2 = secrets_generator.randint(1, 100)
+    correct_answer = math.gcd(num1, num2)
+    expression = '{0} {1}'.format(num1, num2)
     return expression, str(correct_answer)
 
 
 def brain_gcd():
     """Start the brain-gcd game."""
-    game_rule = 'Find the greatest common divisor of given numbers.\n'
-    start_game(game_rule, _brain_gcd_game_function)
+    start_game(GAME_RULE, _game_data_generation)
