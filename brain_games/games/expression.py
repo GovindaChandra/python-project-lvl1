b7b1c9c75a819d67b373397a4f0fd2ad794dcf5c
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-18 21:57:11 +0500

    replasement random module on secrets module, refactoringbrain_calc_game function

--- brain_games/games/expression.py
+++ brain_games/games/expression.py
@@ -1,26 +1,26 @@
 """Expression calculation game logic."""
 
-import random
+import secrets
 
 from brain_games.game_engine.game_engine import start_game
 
 
-def _calc(operands):
-    if operands[1] == '+':
-        return operands[0] + operands[2]
-    elif operands[1] == '-':
-        return operands[0] - operands[2]
-    elif operands[1] == '*':
-        return operands[0] * operands[2]
+def _calc(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
 
 
 def _brain_calc_game_function():
-    first_op = random.randint(1, 100)  # noqa: S311
-    second_op = random.randint(1, 100)  # noqa: S311
-    ops_sign = random.choice(('+', '-', '*'))  # noqa: S311
-    correct_answer = _calc((first_op, ops_sign, second_op))
-    expression = '{0} {1} {2}'.format(first_op, ops_sign, second_op)
-    return expression, str(correct_answer)
+    secrets_generator = secrets.SystemRandom()
+    num1 = secrets_generator.randint(1, 100)
+    num2 = secrets_generator.randint(1, 100)
+    operator = secrets_generator.choice(('+', '-', '*'))
+    correct_answer = _calc(num1, num2, operator)
+    return '{0} {1} {2}'.format(num1, operator, num2), str(correct_answer)
 
 
 def brain_calc():
