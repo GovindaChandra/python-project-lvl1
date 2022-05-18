5d2ffbad6c8ffe5df4c028de4fabea2739a24415
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-18 22:05:28 +0500

    rename second argument in start_game function, update docstrings

--- brain_games/game_engine/game_engine.py
+++ brain_games/game_engine/game_engine.py
@@ -7,17 +7,17 @@ NUMBER_OF_ROUNDS = 3
 GAME_OVER = "'{0}' is wrong answer ;(. Correct answer was '{1}'."
 
 
-def start_game(game_rule, game_function):
+def start_game(game_rule, game_data_function):
     """Call greeting, lunch game function.
 
     Parameters:
         game_rule: Describe game rules
-        game_function: game logic function
+        game_data_function: game logic and data generation function
     """
     name = welcome_user()
     print(game_rule)
     for _ in range(NUMBER_OF_ROUNDS):
-        expression, correct_answer = game_function()
+        expression, correct_answer = game_data_function()
         print('Question:', expression)
         answer = prompt.string('Your answer: ')
         if answer != correct_answer:
