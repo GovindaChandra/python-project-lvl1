bca4e52b626fdd929c96669b9535be6b05001c87
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-21 18:41:31 +0500

    remove return from welcome_user function

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,12 +4,7 @@ import prompt
 
 
 def welcome_user():
-    """Welcome, request user name, print a greeting and return him.
-
-    Returns:
-        name: User name
-    """
+    """Welcome, request user name, print a greeting."""
     print('\nWelcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!\n'.format(name))
-    return name
