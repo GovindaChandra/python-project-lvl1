2e58439a4e4a050d1c9033e999d53fe2f90e9dba
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-21 23:22:38 +0500

    update import after renaming

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,12 +3,12 @@
 """The game of calculating the expression."""
 
 from brain_games.game_engine import start_game
-from brain_games.games import expression
+from brain_games.games import calc
 
 
 def main():
     """Run Expression calculation brain game."""
-    start_game(expression)
+    start_game(calc)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,12 +3,12 @@
 """Game this number is even or not."""
 
 from brain_games.game_engine import start_game
-from brain_games.games import iseven
+from brain_games.games import even
 
 
 def main():
     """Run IS even brain game."""
-    start_game(iseven)
+    start_game(even)
 
 
 if __name__ == '__main__':
