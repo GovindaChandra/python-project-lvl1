e27976bc4d9144d8ec3ea11ac2a55ab7d7c970b9
Sergey Toylyev <doaylnitay@gmail.com>
2022-05-24 17:52:02 +0500

    move progression generation part to function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,12 +5,7 @@ import random
 GAME_RULE = 'What number is missing in the progression?\n'
 
 
-def game_data_generation():
-    """Generate question and correct answer for game.
-
-    Returns:
-        return: a tuple of question and correct answer
-    """
+def _progression_generation():
     members_count = random.randint(5, 10)
     first_num = random.randint(1, 100)
     diff = random.randint(-members_count, members_count)
@@ -18,6 +13,17 @@ def game_data_generation():
     for _ in range(members_count):
         progression.append(first_num)
         first_num += diff
-    correct_answer = progression[abs(diff) - 1]
-    progression[abs(diff) - 1] = '..'
+    return progression
+
+
+def game_data_generation():
+    """Generate question and correct answer for game.
+
+    Returns:
+        return: a tuple of question and correct answer
+    """
+    progression = _progression_generation()
+    index = random.randint(0, len(progression) - 1)
+    correct_answer = progression[index]
+    progression[index] = '..'
     return ' '.join(map(str, progression)), str(correct_answer)
